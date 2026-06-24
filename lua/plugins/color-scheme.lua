5929511e3688cab291f84479bacfb61f63f1545b
ilorez <zobirnajdaoui@gmail.com>
2026-06-24 17:25:28 +0100

    change theme

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -1,16 +1,48 @@
+-- return {
+--   "folke/tokyonight.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("tokyonight").setup({
+--       style = "night", -- Choose between "storm", "night", "moon", or "day"
+--       transparent = true,
+--       styles = {
+--         sidebars = "dark",
+--         floats = "dark",
+--       },
+--     })
+--     vim.cmd([[colorscheme tokyonight]])
+--   end,
+-- }
+
 return {
-  "folke/tokyonight.nvim",
+  "https://codeberg.org/evergarden/nvim.git",
+  name = "evergarden",
   lazy = false,
   priority = 1000,
   config = function()
-    require("tokyonight").setup({
-      style = "night", -- Choose between "storm", "night", "moon", or "day"
-      transparent = true,
-      styles = {
-        sidebars = "dark",
-        floats = "dark",
+    require("evergarden").setup({
+      theme = {
+        variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+        accent = "green",
+      },
+      editor = {
+        transparent_background = true, -- keeping your current transparent preference
+        sign = { color = "none" },
+        float = {
+          color = "mantle",
+          solid_border = false,
+        },
+        completion = {
+          color = "surface0",
+        },
+      },
+      style = {
+        types = { "italic" },
+        keyword = { "italic" },
+        comment = { "italic" },
       },
     })
-    vim.cmd([[colorscheme tokyonight]])
+    vim.cmd([[colorscheme evergarden]])
   end,
 }
